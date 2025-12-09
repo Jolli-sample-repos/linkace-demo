2adb03b25caad05114ae50e4cfa49626d92a7fec
Joe Hart <joe@jeverest.com>
2025-12-08 17:04:16 -0800

    Update content/_meta.ts
    Updated via Jolli repository editor

--- content/_meta.ts
+++ content/_meta.ts
@@ -1,18 +1,42 @@
-export default {
-  'index': 'Home',
-  'link-checks': 'Link Checks',
-  'links': 'Links',
-  'the-dashboard': 'The Dashboard',
-  'setup-with-php': 'Setup with PHP',
-  'setup-with-docker': 'Setup with Docker',
-  'third-party-tools-and-libraries': 'Third Party Tools and Libraries',
-  'troubleshooting': 'Troubleshooting',
-  'v2-changelog': 'v2 Changelog',
-  'linkace-translations': 'LinkAce Translations',
-  'likeace-overview': 'LikeAce Overview',
-  'api-linkace-openapi': {
-    title: 'Linkace Openapi',
-    type: 'page',
-    href: '/api-docs-linkace-openapi.html'
+export default 
+{
+  "index": {
+    "title": "Getting Started",
+    "type": "separator"
+  },
+  "setup-with-php": "Setup with PHP",
+  "setup-with-docker": "Setup with Docker",
+
+  "using": {
+    "title": "Using LinkAce",
+    "type": "separator"
+  },
+  "the-dashboard": "The Dashboard",
+  "links": "Links",
+  "link-checks": "Link Checks",
+
+  "guides": {
+    "title": "Guides",
+    "type": "separator"
+  },
+  "third-party-tools-and-libraries": "Third Party Tools and Libraries",
+  "troubleshooting": "Troubleshooting",
+
+  "reference": {
+    "title": "Reference",
+    "type": "separator"
+  },
+  "v2-changelog": "v2 Changelog",
+  "linkace-translations": "LinkAce Translations",
+  "likeace-overview": "LikeAce Overview",
+
+  "api": {
+    "title": "API Documentation",
+    "type": "separator"
+  },
+  "api-linkace-openapi": {
+    "title": "LinkAce OpenAPI",
+    "type": "page",
+    "href": "/api-docs-linkace-openapi.html"
   }
 }
