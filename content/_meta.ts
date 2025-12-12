f184986dc9d93b5f076ecfdd17b1822d45ed0835
Joe Hart <joe@jeverest.com>
2025-12-12 15:29:23 -0800

    Update documentation (preserve custom files)

--- content/_meta.ts
+++ content/_meta.ts
@@ -2,31 +2,14 @@ export default {
   'index': { display: 'hidden' },
   'setup-with-php': 'Setup with PHP',
   'setup-with-docker': 'Setup with Docker',
-  'using': {
-    type: 'separator',
-    title: 'Using LinkAce'
-  },
   'the-dashboard': 'The Dashboard',
   'links': 'Links',
   'link-checks': 'Link Checks',
-  'guides': {
-    type: 'separator',
-    title: 'Guides'
-  },
   'third-party-tools-and-libraries': 'Third Party Tools and Libraries',
   'troubleshooting': 'Troubleshooting',
-  'reference': {
-    type: 'separator',
-    title: 'Reference'
-  },
   'v2-changelog': 'v2 Changelog',
   'linkace-translations': 'LinkAce Translations',
   'likeace-overview': 'LinkAce Overview',
-  'linkace-docs': {
-    title: 'Docs',
-    type: 'page',
-    href: '/'
-  }
   'api-reference': {
     title: 'API Reference',
     type: 'page',
